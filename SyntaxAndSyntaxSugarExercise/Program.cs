ec64b3058f8f70ed8d16bc702ea5859db393561d
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-13 16:16:10 -0700

    Syntax exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,3 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿var answer = 19;
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+Console.WriteLine(response);
